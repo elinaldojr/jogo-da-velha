b3d16ecf0ce41eb7439b22a3bd550fa800c6016e
Elinaldo Jr <esgjunior@uesc.br>
2022-03-31 09:01:36 -0300

    refatora função 'verifica vencedor'

--- velha.js
+++ velha.js
@@ -224,7 +224,7 @@ function checaVencedor(){
         escreverFrase(frase, 'comentario');
     }
     
-    if (quadrado1.innerHTML !== '' && quadrado2.innerHTML !== '' && quadrado3.innerHTML !== '' &&
+    else if (quadrado1.innerHTML !== '' && quadrado2.innerHTML !== '' && quadrado3.innerHTML !== '' &&
         quadrado4.innerHTML !== '' && quadrado5.innerHTML !== '' && quadrado6.innerHTML !== '' &&
         quadrado7.innerHTML !== '' && quadrado8.innerHTML !== '' && quadrado9.innerHTML !== ''){
             jogadorSelecionado.innerHTML = '';
